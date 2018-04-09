82b2c504a65bf89b01679182c2220eda063a6bae
Jack Schaffner <jackwschaffner@gmail.com>
2018-04-09 01:49:55 -0400

    Working registration and sessions.

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 var request = require('request');
+var config = require('./config.json');
+var fs = require('fs');
 var self = {
-    id: -1,
+    id: config.id,
+    sessionKey: config.sessionKey,
     target: null
 }
 var heartbeatInterval = null;
 var api = 'http://zeroparticle.net:3000';
-//api = 'http://127.0.0.1:3000';
+api = 'http://127.0.0.1:3000';
 
 function heartbeat() {
     console.log('Sending heartbeat.');
@@ -25,17 +28,25 @@ function heartbeat() {
 }
 
 function runClient() {
+    console.log("Registering with id " + self.id + " and session key " + self.sessionKey);
     request({
         url: api + '/register',
         method: "POST",
         json: self
     }, function (err, res, body) {
-        if (!res || res.statusCode != 200) {
+        if (res && res.statusCode == 403) {
+            console.log("Backend refused register request. The configured id may be in use.");
+        } else if (!res || res.statusCode != 200) {
             console.log("Error registering with backend.");
         } else {
-            console.log('Registered with backend.');
-            self.id = body.id;
-            console.log('Updated id to: ' + self.id);
+            console.log('Registered with backend as ' + self.id);
+            self.sessionKey = body.sessionKey;
+            var updatedConf = {
+                id: self.id,
+                sessionKey: self.sessionKey
+            }
+            fs.writeFileSync('config.json', JSON.stringify(updatedConf));
+            console.log('Using session key ' + self.sessionKey);
             heartbeatInterval = setInterval(heartbeat, 1000);
         }
     });
