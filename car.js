ba7dee1be164b561f4b53d96f58a76b3f71d230d
Thomas Barnette <tvbarnette999@gmail.com>
2018-04-15 20:21:53 +0000

    worked out logic for pivoting in any direction based on IR detectors

--- car.js
+++ car.js
@@ -62,19 +62,6 @@ function stop(b, a1, a2, b1, b2, pa, pb) {
   b.analogWrite(pb, 0);  
   
 }
-  
-/**function turnClockwise90(b, a1, a2, b1, b2, pa, pb) {
-  b.digitalWrite(a1, b.HIGH);
-  b.digitalWrite(a2, b.LOW);
-  b.analogWrite(pa, 1);
-
-  b.digitalWrite(b1, b.LOW);
-  b.digitalWrite(b2, b.HIGH);
-  b.analogWrite(pb, 0);
-  setTimeout ( function() {
-    stop(b, a1, a2, b1, b2, pa, pb);
-  }, 525);
-}*/
 
 /**function pivot(b, a1, a2, b1, b2, pa, pb, deg, det1, det2, det3, det4) {
   if (deg > 0) {
@@ -96,32 +83,95 @@ function stop(b, a1, a2, b1, b2, pa, pb) {
   }*/
   
 function pivot(b, a1, a2, b1, b2, pa, pb, det) {
-  if (det.indexOf(1) != 0) {
+  if (det[1] != 0) {
     stop(b, a1, a2, b1, b2, pa, pb);
-  }
-  if (det.indexOf(0) >= 0 || det.indexOf(0) <= 2)
+  } 
+  if (det[0] == 0) {
+    forward(b, a1, a2, b1, b2, pa, pb);
+  } else if (det[0] == 1) {
     b.digitalWrite(a1, b.HIGH);
     b.digitalWrite(a2, b.LOW);
     b.analogWrite(pa, 1);
 
     b.digitalWrite(b1, b.LOW);
     b.digitalWrite(b2, b.HIGH);
-    b.analogWrite(pb, deg/180);
-  } else {
+    b.analogWrite(pb, .5);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 250);
+  } else if (det[0] == 2){
+    b.digitalWrite(a1, b.HIGH);
+    b.digitalWrite(a2, b.LOW);
+    b.analogWrite(pa, 1);
+
+    b.digitalWrite(b1, b.LOW);
+    b.digitalWrite(b2, b.HIGH);
+    b.analogWrite(pb, .5);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 400);
+  } else if (det[0] == 3){
+    b.digitalWrite(a1, b.HIGH);
+    b.digitalWrite(a2, b.LOW);
+    b.analogWrite(pa, 1);
+
+    b.digitalWrite(b1, b.LOW);
+    b.digitalWrite(b2, b.HIGH);
+    b.analogWrite(pb, .5);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 500);
+  } else if (det[0] == 4){
+    b.digitalWrite(a1, b.HIGH);
+    b.digitalWrite(a2, b.LOW);
+    b.analogWrite(pa, 1);
+
+    b.digitalWrite(b1, b.LOW);
+    b.digitalWrite(b2, b.HIGH);
+    b.analogWrite(pb, .5);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 625);
+  } else if (det[0] == -1){
     b.digitalWrite(a1, b.LOW);
     b.digitalWrite(a2, b.HIGH);
-    b.analogWrite(pa, deg/180);
+    b.analogWrite(pa, .5);
 
     b.digitalWrite(b1, b.HIGH);
     b.digitalWrite(b2, b.LOW);
     b.analogWrite(pb, 1);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 250);
+  } else if (det[0] == -2){
+    b.digitalWrite(a1, b.LOW);
+    b.digitalWrite(a2, b.HIGH);
+    b.analogWrite(pa, .5);
+
+    b.digitalWrite(b1, b.HIGH);
+    b.digitalWrite(b2, b.LOW);
+    b.analogWrite(pb, 1);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 400);
+  } else if (det[0] == -3){
+    b.digitalWrite(a1, b.LOW);
+    b.digitalWrite(a2, b.HIGH);
+    b.analogWrite(pa, .5);
+
+    b.digitalWrite(b1, b.HIGH);
+    b.digitalWrite(b2, b.LOW);
+    b.analogWrite(pb, 1);
+    setTimeout(function() {
+      stop(b, a1, a2, b1, b2, pa, pb);
+    }, 625);
   }
   
 }
 
 //forward(b, a1, a2, b1, b2, pa, pb);
 //reverse(b, a1, a2, b1, b2, pa, pb);
-pivot(b, a1, a2, b1, b2, pa, pb, -90);
+var arg = [4, 0];
+pivot(b, a1, a2, b1, b2, pa, pb, arg);
 //setTimeout(function (){forward(b, a1, a2, b1, b2, pa, pb)}, 500);
-setTimeout(function (){stop(b, a1, a2, b1, b2, pa, pb)}, 500);
-//turnClockwise90(b, a1, a2, b1, b2, pa, pb);
+//setTimeout(function (){stop(b, a1, a2, b1, b2, pa, pb)}, 500);
