17349664e9c51cd1c4af8450f3d4ea1fe4fa8f12
Thomas Barnette <tvbarnette999@gmail.com>
2018-04-15 19:32:48 +0000

    added functionality to pivot according to detector.js

--- car.js
+++ car.js
@@ -10,7 +10,7 @@ var a1='P9_23';
 var a2='P9_17';
 var b1='P9_24';
 var b2='P9_26';
-var oe='P9_41';//P9_30
+var oe='P9_41';//used to be P9_30
 
 b.pinMode(a1, b.OUTPUT);
 b.pinMode(a2, b.OUTPUT);
@@ -76,29 +76,52 @@ function stop(b, a1, a2, b1, b2, pa, pb) {
   }, 525);
 }*/
 
-function pivot(b, a1, a2, b1, b2, pa, pb, deg) {
+/**function pivot(b, a1, a2, b1, b2, pa, pb, deg, det1, det2, det3, det4) {
   if (deg > 0) {
     b.digitalWrite(a1, b.HIGH);
     b.digitalWrite(a2, b.LOW);
     b.analogWrite(pa, 1);
 
-    b.digitalWrite(b1, b.HIGH);
-    b.digitalWrite(b2, b.LOW);
-    b.analogWrite(pb, deg/360);
-  /**} else {
+    b.digitalWrite(b1, b.LOW);
+    b.digitalWrite(b2, b.HIGH);
+    b.analogWrite(pb, deg/180);
+  } else {
     b.digitalWrite(a1, b.LOW);
     b.digitalWrite(a2, b.HIGH);
-    b.analogWrite(pa, deg/360);
+    b.analogWrite(pa, deg/180);
 
     b.digitalWrite(b1, b.HIGH);
     b.digitalWrite(b2, b.LOW);
-    b.analogWrite(pb, 1);*/
+    b.analogWrite(pb, 1);
+  }*/
+  
+function pivot(b, a1, a2, b1, b2, pa, pb, det) {
+  if (det.indexOf(1) != 0) {
+    stop(b, a1, a2, b1, b2, pa, pb);
+  }
+  if (det.indexOf(0) >= 0 || det.indexOf(0) <= 2)
+    b.digitalWrite(a1, b.HIGH);
+    b.digitalWrite(a2, b.LOW);
+    b.analogWrite(pa, 1);
+
+    b.digitalWrite(b1, b.LOW);
+    b.digitalWrite(b2, b.HIGH);
+    b.analogWrite(pb, deg/180);
+  } else {
+    b.digitalWrite(a1, b.LOW);
+    b.digitalWrite(a2, b.HIGH);
+    b.analogWrite(pa, deg/180);
+
+    b.digitalWrite(b1, b.HIGH);
+    b.digitalWrite(b2, b.LOW);
+    b.analogWrite(pb, 1);
   }
   
 }
 
 //forward(b, a1, a2, b1, b2, pa, pb);
-pivot(b, a1, a2, b1, b2, pa, pb, 180);
-setTimeout(function (){forward(b, a1, a2, b1, b2, pa, pb)}, 500);
-setTimeout(function (){stop(b, a1, a2, b1, b2, pa, pb)}, 1000);
+//reverse(b, a1, a2, b1, b2, pa, pb);
+pivot(b, a1, a2, b1, b2, pa, pb, -90);
+//setTimeout(function (){forward(b, a1, a2, b1, b2, pa, pb)}, 500);
+setTimeout(function (){stop(b, a1, a2, b1, b2, pa, pb)}, 500);
 //turnClockwise90(b, a1, a2, b1, b2, pa, pb);
