9c586004d506bdaffb1bc44abd8b142cfffbae61
aegorov <aegorov@ncsu.edu>
2018-04-23 16:45:23 -0400

    Add code for ultrasonic sensor?

---
+++ ultrasonic/ultrasonic.js
@@ -0,0 +1,41 @@
+var b = require('bonescript');
+
+var trigger = 'P9_16',
+	echo 	= 'P9_41',
+	ms 	= 250;
+
+var startTime, pulseTime;
+
+b.pinMode(echo, b.INPUT, 7, 'pulldown', 'fast', doAttach);
+function doAttach(x) {
+	if (x.err) {
+		console.log('x.err =', x.err);
+		return;
+	} else {
+		console.log('it"s all good');
+	}
+	b.attachInterrupt(echo, true, b.FALLING, interruptCallback);
+}
+b.pinMode(trigger, b.OUTPUT);
+b.digitalWrite(trigger, 1);
+
+setInterval(ping, ms);
+
+function ping() {
+	console.log('ping');
+	b.digitalWrite(trigger, 0);
+	startTime = process.hrtime();
+}
+
+function interruptCallback(x) {
+	if (x.attached) {
+		console.log('Interrupt handler attached', x);
+		return;
+	}
+	console.log('hello?')
+	if (startTime) {
+		pulseTime = process.hrtime(startTime);
+		b.digitalWrite(trigger, 1);
+		console.log('pulseTime = ' + (pulseTime[1] / 1000000 - 0.8).toFixed(3));
+	}
+}
