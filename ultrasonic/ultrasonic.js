60407b98ea37fe88ab1f810e4fbd6a5a3030527a
aegorov <aegorov@ncsu.edu>
2018-04-24 18:26:08 -0400

    Get rid of console.log in ultrasonic.js

--- ultrasonic/ultrasonic.js
+++ ultrasonic/ultrasonic.js
@@ -1,7 +1,7 @@
 var b = require('bonescript');
 
-var trigger = 'P8_17', //outpin
-	echo 	= 'P8_19', //inpin
+var trigger = 'P8_17',
+	echo 	= 'P8_19',
 	ms 	= 250;
 
 var startTime, pulseTime;
@@ -11,8 +11,6 @@ function doAttach(x) {
 	if (x.err) {
 		console.log('x.err =', x.err);
 		return;
-	} else {
-		console.log('it"s all good');
 	}
 	b.attachInterrupt(echo, true, b.FALLING, interruptCallback);
 }
@@ -22,17 +20,14 @@ b.digitalWrite(trigger, 1);
 setInterval(ping, ms);
 
 function ping() {
-	console.log('ping');
 	b.digitalWrite(trigger, 0);
 	startTime = process.hrtime();
 }
 
 function interruptCallback(x) {
 	if (x.attached) {
-		console.log('Interrupt handler attached', x);
 		return;
 	}
-	console.log('hello?')
 	if (startTime) {
 		pulseTime = process.hrtime(startTime);
 		b.digitalWrite(trigger, 1);
