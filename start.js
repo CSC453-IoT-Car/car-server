cc408a4fab162bfc31331e6c67b14b682232cd53
Jack Schaffner <JackWSchaffner@gmail.com>
2018-04-26 22:28:17 -0400

    oops

--- start.js
+++ start.js
@@ -1,10 +1,9 @@
 const exec = require('child_process').exec;
 function waitMore() {
     console.log('waiting more...');
+    setTimeout(waitMore(), 1000000);
 }
 var startScript = exec('bash /home/debian/car-server/start-server.sh', (error, stdout, stderr) => {
     console.log('Done.');
-    while (true) {
-        setTimeout(waitMore(), 1000000);
-    }
+    setTimeout(waitMore(), 1000000);
 });
