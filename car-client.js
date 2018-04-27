89c7d0d9ab0fd20df640830a9f8fd8ee93e929ac
ashncook <36202111+ashncook@users.noreply.github.com>
2018-04-26 21:25:30 -0400

    added full avoidance logic

--- car-client.js
+++ car-client.js
@@ -42,7 +42,6 @@ var prevDistArray = new Array(3);
 prevDistArray[0] = 9999;
 prevDistArray[1] = 999;
 prevDistArray[2] = 999;
-var arrayIndex = 0;
 
 function toolsSetup() {
     //Car
@@ -80,12 +79,61 @@ function toolsSetup() {
         if (startTime) {
             pulseTime = process.hrtime(startTime);
             b.digitalWrite(pins.trigger, 1);
-		    prevDistArray[0] = prevDistArray[1];
-		    prevDistArray[1] = prevDistArray[2];
-		    prevDistArray[2] = (pulseTime[1] / 1000000 - 0.8).toFixed(3);
+		//added from ultrasonicMovement.js
+		distance = (pulseTime[1] / 1000000 - 0.8).toFixed(3)
+		console.log('distance', distance);
+		if (distance > 3.0 && (prevDistArray[2] > 3.0)) {
+            car.forward(a1, a2, b1, b2, pa, pb);
+			
+		} else if (distance <= 3.0 && (prevDistArray[2] <= 3.0)) {
+            car.stop(a1, a2, b1, b2, pa, pb);
+            b.detachInterrupt(echo, avoidance);
+        	
+        	//setTimeout(car.stop, 100);
+            
+		}
+		prevDistArray[0] = prevDistArray[1];
+		prevDistArray[1] = prevDistArray[2];
+		prevDistArray[2] = distance;
+		console.log("Post Shift: "+prevDistArray);
         }
     }
-
+    //added from ultrasonicMovement.js
+    function avoidance(x) {
+	    var det = new Array(2);
+	det[0] = 2;
+	det[1] = 0;
+	
+	var det2 = new Array(2);
+    det2[0] = -2;
+    det2[1] = 0;
+        	
+    car.stop(a1,a2,b1,b2,pa,pb);
+    setTimeout(function(){
+	    car.pivot(a1, a2, b1, b2, pa, pb, det);
+	        	
+	    setTimeout(function (){
+	    	car.stop(a1, a2, b1, b2, pa, pb);
+	    	setTimeout(function(){
+		    	car.forward(a1, a2, b1, b2, pa, pb);
+		    	setTimeout( function(){
+		    		car.stop(a1, a2, b1, b2, pa, pb);
+		    		setTimeout(function(){
+			    		car.pivot(a1, a2, b1, b2, pa, pb, det2);
+			    			setTimeout(function(){
+			    				prevDistArray[0] = 9999;
+								prevDistArray[1] = 999;
+								prevDistArray[2] = 999;
+			    				b.attachInterrupt(echo, true, b.FALLING, interruptCallback);
+			    				b.digitalWrite(trigger, 1);
+			    			}, 1000);
+		    		}, 1000);
+		    	}, 2000);
+	    	}, 500);
+	    }	, 2000);
+    }, 500);
+    }
+	    
 }
 
 // Car movements
