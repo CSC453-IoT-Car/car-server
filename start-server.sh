f5d471ff9304b6893b86612ad1a65fdfbc796eda
Jack Schaffner <JackWSchaffner@gmail.com>
2018-04-26 22:45:37 -0400

    env variables

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [ "$CAR" = "started" ]
+then
+else
+export CAR=started
 screen -dmS car bash -c "cd /home/debian/car-server/; echo temppwd | sudo -S ./setup_pru.sh; echo temppwd | sudo -S node client.js; exec sh"
+fi
