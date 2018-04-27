4529a2870463c7697c09047e4ca8f9c95d45a4bb
ashncook <36202111+ashncook@users.noreply.github.com>
2018-04-26 21:46:03 -0400

    changed speed and took out useless function

--- car.js
+++ car.js
@@ -26,11 +26,11 @@ module.exports = {
   forward: function(a1, a2, b1, b2, pa, pb){
     b.digitalWrite(a1, b.HIGH);
     b.digitalWrite(a2, b.LOW);
-    b.analogWrite(pa, .40);
+    b.analogWrite(pa, 0.40);
   
     b.digitalWrite(b1, b.HIGH);
     b.digitalWrite(b2, b.LOW);
-    b.analogWrite(pb, 0.35);
+    b.analogWrite(pb, 0.40);
   },
   
   reverse: function(a1, a2, b1, b2, pa, pb) {
@@ -146,70 +146,9 @@ module.exports = {
       b.analogWrite(pb, 1);
       setTimeout(function() {
         this.stop(a1, a2, b1, b2, pa, pb);
-      }, 625);
+      }, 640);
     }
   
-  },
-  
-  /**
-   * Avoid objects without consulting the backend.
-   **/
-  objectAvoidance: function(a1, a2, b1, b2, pa, pb, trigger, echo){
-    var car = require('./car.js');
-    //from ultrasonicMovement.js
-    var ms = 250;
-    var startTime, pulseTime;
-
-    b.pinMode(echo, b.INPUT, 7, 'pulldown', 'fast', doAttach);
-    function doAttach(x) {
-	    if (x.err) {
-		    console.log('x.err =', x.err);
-		    return;
-	    }
-	    b.attachInterrupt(echo, true, b.FALLING, interruptCallback);
-    }
-    
-    b.pinMode(trigger, b.OUTPUT);
-    b.digitalWrite(trigger, 1);
-
-    var prevDist = 0;
-
-    var start = true;
-
-    var interval = setInterval(ping, ms);
-    var distance = 0;
-
-    function ping() {
-	    b.digitalWrite(trigger, 0);
-	    startTime = process.hrtime();
-    }
-
-    function interruptCallback(x) {
-	    if (x.attached) {
-		    return;
-	    }
-	    if (startTime) {
-		    pulseTime = process.hrtime(startTime);
-		    b.digitalWrite(trigger, 1);
-		    distance = (pulseTime[1] / 1000000 - 0.8).toFixed(3)
-		    console.log('distance', distance);
-		    if (distance > 3.25 && (Math.abs(prevDist - distance) <= 1 || start)) {
-		      car.forward(a1, a2, b1, b2, pa, pb);
-		      prevDist = distance;
-			    start = false;
-		    } else if (prevDist - distance >= 0){
-          //added to avoid objects and try again.
-          prevDist = distance;
-          car.stop(a1, a2, b1, b2, pa, pb);
-          var det = [2,1];
-          car.pivot(a1, a2, b1, b2, pa, pb, det);
-          car.forward(a1, a2, b1, b2, pa, pb);
-          det = [-2,1];
-          car.pivot(a1, a2, b1, b2, pa, pb, det);
-          car.objectAvoidance(a1, a2, b1, b2, pa, pb, trigger, echo);
-		    }
-	    }
-    }
   }
 
 }
