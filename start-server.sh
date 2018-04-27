3b94227926f252c640e17872e874344c42a03911
Jack Schaffner <JackWSchaffner@gmail.com>
2018-04-27 06:14:29 -0400

    undo

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if ! screen -list | grep -q "car"; then
-    screen -dmS car bash -c "export NODE_PATH=/usr/local/lib/node_modules; cd /home/debian/car-server/; echo temppwd | sudo -S ./setup_pru.sh; echo temppwd | sudo -S node car-client.js; exec sh"
+    screen -dmS car bash -c "cd /home/debian/car-server/; echo temppwd | sudo -S ./setup_pru.sh; echo temppwd | sudo -S node car-client.js; exec sh"
 fi
